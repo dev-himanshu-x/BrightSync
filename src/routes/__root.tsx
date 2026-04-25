f2bb46af4b4dd89887a54feff38094036ef9e88c
Himanshu <iam.himanshu.x@gmail.com>
2026-04-26 01:46:46 +0530

    feat: add Vercel Analytics integration to track user interactions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { Analytics } from '@vercel/analytics/react'
 
 import appCss from '../styles.css?url'
 
@@ -34,6 +35,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Analytics />
         <Scripts />
       </body>
     </html>
